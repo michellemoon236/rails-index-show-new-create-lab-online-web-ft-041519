e943fc800f82fdd732f4e6e4df344352540f509c
Michelle Moon <michellemoon236@gmail.com>
2019-06-19 04:23:54 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,15 @@
 class CouponsController < ApplicationController
   
+  def index
+    
+  end
+  
+  def new 
+    @coupon = Coupon.find
+  end
+  
+  def create 
+    @coupon = Coupon.new
+  end
+  
 end
